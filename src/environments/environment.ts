72232a7b6d9eaee9b847009fe64b8c2406f3a725
Nicorici Ioan <honnigan18@gmail.com>
2024-05-21 17:06:45 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    apiKey: 'AIzaSyAmE1dEH_KG2X_gfucI-Dc7iQjSGw0Gf9c',
+    authDomain: 'angular-eb084.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    projectId: 'angular-eb084',
+    storageBucket: 'angular-eb084.appspot.com',
+    messagingSenderId: '1063502781890',
+    appId: '1:1063502781890:web:5cc03e561482664bd18a5b',
+    measurementId: 'G-8DC233QGQ3',
   },
 };
